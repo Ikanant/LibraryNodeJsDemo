d2b27cf747b49690a190c81b888dcca035a7495d
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-03-07 21:37:42 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+var port = 5000;
+
+app.get('/', function(req, res){
+    res.send('public/index.html');
+});
+
+app.listen(port, function(err){
+   console.log("Running server on port: " + port);
+});

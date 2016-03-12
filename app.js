fc40901470b9451c32918c874cff33a3b10b37c9
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-03-12 15:16:23 -0500

    MongoDB added and EJS templating engine utilized

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
 
+var nav = require('./src/tools/navigationBar');
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')();
+
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function(req, res){
-    res.render('index'); 
+    res.render('index', {title: 'EJS Title', nav: [{Link:'Books', Text:'Books'}, {Link: 'Authors', Text: 'Authors'}]});
 });
 
 app.listen(port, function(err){

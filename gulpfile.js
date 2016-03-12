4e6d95b41dc2760378e2c133a71a33aa32e763ab
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-03-12 09:26:53 -0500

    edited gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,28 @@ gulp.task('style', function(){
         }))
         .pipe(jscs());
 });
+
+gulp.task('inject', function(){
+    var wiredep = require('wiredep').stream;
+
+    var options = {
+        bowerJson: require('./bower.json'), //Require will return the json object for future use
+        directory: './public/lib',
+        ignorePath: '../../public'
+    }
+
+
+    // We are going to use gulp-inject for our css and js files (not bower)
+    var inject  = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                                './public/js/*.js'], {read: false}); //Read false because we only need their names, not their content
+    var injectOptions = {
+      ignorePath: '/public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
